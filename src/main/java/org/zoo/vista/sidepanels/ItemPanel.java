5b44239a673a96f28f0e9b2c9db6b21a57e16703
Antonio Skorin <antonio.skorin@gmail.com>
2023-11-25 01:03:01 -0300

    Fix: Flechas de navegación mejor posicionadas.

--- src/main/java/org/zoo/vista/sidepanels/ItemPanel.java
+++ src/main/java/org/zoo/vista/sidepanels/ItemPanel.java
@@ -9,6 +9,7 @@ public class ItemPanel extends JPanel {
     public ItemPanel(VistaEscenaZoo.PanelListener panelListener) {
         super();
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        add(Box.createHorizontalStrut(50));
     }
 
     public void addNavArrow(int width, int height, VistaEscenaZoo.PanelListener panelListener) {
@@ -17,5 +18,6 @@ public class ItemPanel extends JPanel {
         labelNavArrow.addMouseListener(panelListener);
         add(Box.createGlue());
         add(labelNavArrow);
+        add(Box.createHorizontalStrut(50));
     }
 }
