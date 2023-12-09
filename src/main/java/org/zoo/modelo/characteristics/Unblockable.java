c4df0af15ec0e3e3a55822ca9050f328587c47d8
molivas2022 <molivas2022@inf.udec.cl>
2023-12-09 20:53:52 -0300

    Documentacion characteristics

--- src/main/java/org/zoo/modelo/characteristics/Unblockable.java
+++ src/main/java/org/zoo/modelo/characteristics/Unblockable.java
@@ -2,8 +2,21 @@ package org.zoo.modelo.characteristics;
 
 import org.zoo.utilities.Hitbox;
 
+/**
+ * Interfaz cuyos elementos que la implementan son aquellos que
+ * impiden que otros objetos se sobrepongan a ellos
+ */
 public interface Unblockable {
+    /**
+     * Permite obtener la hitbox del objeto relativa al contenedor mas inmediato que lo contenga
+     * @return Hitbox con posicion relativa y tamaño
+     */
     Hitbox getHitbox();
+
+    /**
+     * Permite obtener la hitbox absoluta del objeto, es decir, en relacion al zoo
+     * @return Hitbox con posicion absoluta y tamaño
+     */
     Hitbox getAbsHitbox();
 }
 
