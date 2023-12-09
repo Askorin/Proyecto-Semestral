408375a7ce2af8b12fd0ce1cc702f458a8d1bcb8
Antonio Skorin <antonio.skorin@gmail.com>
2023-12-08 23:56:41 -0300

    Docs: Documentación añadida.

--- src/main/java/org/zoo/modelo/MenuItem.java
+++ src/main/java/org/zoo/modelo/MenuItem.java
@@ -1,6 +1,9 @@
 package org.zoo.modelo;
 
 
+/**
+ * A ser implementado por un elemento que se encuentre en el menu y deba ser mostrado en el.
+ */
 public interface MenuItem {
     public Sprite getInGameSprite();
 
