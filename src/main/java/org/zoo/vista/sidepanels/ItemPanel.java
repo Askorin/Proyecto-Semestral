f301fc86c7c4c11d764eef7ac4977617480f7cf1
Antonio Skorin <antonio.skorin@gmail.com>
2023-12-09 00:34:24 -0300

    Fix: gitignore

--- src/main/java/org/zoo/vista/sidepanels/ItemPanel.java
+++ src/main/java/org/zoo/vista/sidepanels/ItemPanel.java
@@ -100,7 +100,7 @@ public class ItemPanel<T extends Enum<T> & MenuItem> extends JPanel {
     public Dimension getPreferredSize() {
         float ratio = getWidth() / (float) bgDim.width;
         int panelWidth = (int) (bgDim.width * ratio);
-        // TODO: tuve que multiplicar por 0.6 :(. Arreglar o usar otro panelbg
+        // TODO: tuve que multiplicar por 0.6 :( 
         int panelHeight = (int) (bgDim.height * ratio * 0.6);
         return new Dimension(panelWidth, panelHeight);
     }
