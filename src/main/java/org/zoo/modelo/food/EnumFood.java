5eab788a5fdde805f5182d1d9fdd3f778eccbf41
molivas2022 <molivas2022@inf.udec.cl>
2023-12-09 21:40:50 -0300

    Documentacion de comidas

--- src/main/java/org/zoo/modelo/food/EnumFood.java
+++ src/main/java/org/zoo/modelo/food/EnumFood.java
@@ -3,11 +3,17 @@ package org.zoo.modelo.food;
 import org.zoo.modelo.MenuItem;
 import org.zoo.modelo.Sprite;
 
+/**
+ * Enumeracion que modela las comidas que maneja el programa
+ */
 public enum EnumFood implements MenuItem {
     FISH(Sprite.FISH_INGAME),
     LEAVES(Sprite.LEAVES_INGAME),
     MEAT(Sprite.MEAT_INGAME),
     BANANA(Sprite.BANANA_INGAME);
+    /**
+     * Informacion grafica (solo logica, no contiene una imagen) de la comida dentro del zoo
+     */
     private final Sprite inGameSprite;
     EnumFood(Sprite inGameSprite) {
         this.inGameSprite = inGameSprite;
